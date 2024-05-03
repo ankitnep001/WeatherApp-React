96b337ed3386bc06f9ff67877a354bff44891bf9
Ankit <ankit001nep@gmail.com>
2024-05-03 15:57:12 +0545

    favicon-added

--- src/Components/HeroLeft.tsx
+++ src/Components/HeroLeft.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
+// interface Coord{
+
+// }
+
 const Hero: React.FC = () => {
+
     return (
         <div className='bg-gray-300 w-[65dvw] h-[90vh] mx-10 flex justify-center items-center'>
             weather content
--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from '../Components/Header'
-import HeroLeft from '../Components/HeroLeft'
-import HeroRight from '../Components/HeroRight'
+import Header from '../components/Header'
+import HeroLeft from '../components/HeroLeft'
+import HeroRight from '../components/HeroRight'
 const Home: React.FC = () => {
     return (
         <>
