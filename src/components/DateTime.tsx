0784e0f1972f2196648029e0ec5f4d90d065fd65
Ankit <ankit001nep@gmail.com>
2024-05-13 10:11:54 +0545

    Nepali Date (DateTime.tsx)

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -1,19 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { MdDateRange } from "react-icons/md";
-const DateTime: React.FC = () => {
-    const [date, setDate] = useState<number>(Date.now());
+import BikramSambat from "bikram-sambat-js"
 
-    useEffect(() => {
-        const time = setInterval(() => {
-            setDate(Date.now());
-        }, 1000)
-        return () => {
-            clearInterval(time);
-        };
-    })
+const DateTime: React.FC = () => {
+    const currentDate = new Date();
+    const nepaliDate = new BikramSambat(currentDate).toBS();
+
     return (
         <div className='flex justify-center md:justify-end py-2 mx-10'>
-            <MdDateRange size={25} />{new Date(date).toLocaleDateString()}
+            <MdDateRange size={25} />{nepaliDate}
         </div>
     )
 }
