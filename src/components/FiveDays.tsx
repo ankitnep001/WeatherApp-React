b859aaa7680026730b6e1e30471ba816fc5381aa
Ankit <ankit001nep@gmail.com>
2024-05-15 14:34:32 +0545

    incremented date(FiveDays.tsx)

--- src/components/FiveDays.tsx
+++ src/components/FiveDays.tsx
@@ -1,5 +1,8 @@
-import { useState, useEffect } from "react";
-// import BikramSambat, { ADToBS, BSToAD } from "bikram-sambat-js"
+import { LanguageContext } from "@context/LanguageContext";
+import { useState, useEffect, useContext } from "react";
+import BikramSambat from "bikram-sambat-js"
+import { convertToNepaliNumbers } from 'utils/nepalinumber';
+
 interface DailyWeather {
     list: {
         main: {
@@ -18,9 +21,10 @@ interface DailyProps {
 }
 const FiveDays: React.FC<DailyProps> = ({ location }) => {
     const [hourlyWeather, setHourlyWeather] = useState<DailyWeather | null>(null);
-    // const [nepaliDate, setNepaliDate] = useState<string>('');
+    const { dictionary } = useContext(LanguageContext)
     useEffect(() => {
         const fetchData = async () => {
+
             try {
                 const response = await fetch(`${import.meta.env.VITE_WEATHER_API_URL}/forecast?q=${location}&appid=${import.meta.env.VITE_API_KEY}`);
                 if (!response.ok) throw new Error('Failed to fetch weather data');
@@ -36,38 +40,36 @@ const FiveDays: React.FC<DailyProps> = ({ location }) => {
     }, [location]);
 
 
+    const { language } = useContext(LanguageContext);
 
-    const [date, setDate] = useState<number>(Date.now());
-
-    useEffect(() => {
-        const time = setInterval(() => {
-            setDate(Date.now());
-        }, 1000)
-        return () => {
-            clearInterval(time);
-        };
-    })
 
     return (
         <>
-            <p className="text-center">5 Days Weather</p>
+            <p className="text-center">{dictionary.fiveDays}</p>
             <div className='mx-10 flex flex-wrap justify-center items-center '>
 
                 {hourlyWeather && hourlyWeather.list.slice(1, 6).map((hourlyData, index) => {
-                    const currentDate = new Date(date);
-                    currentDate.setDate(currentDate.getDate() + index + 1); //date increase
-                    // const nextDate = getNextNepaliDate(nepaliDate, index + 1); 
+
+                    const currentDate = new Date();
+                    currentDate.setDate(currentDate.getDate() + index + 1); // Increase date for each day
+                    const nepaliEngDate = new BikramSambat(currentDate).toBS();
+                    let nepaliDate;
+
+                    if (language === 'ne') {
+                        nepaliDate = convertToNepaliNumbers(nepaliEngDate);
+                    } else {
+                        nepaliDate = nepaliEngDate;
+                    }
                     return (
                         <div
                             className=" flex flex-col justify-center items-center  m-2  w-40 h-28 rounded-lg shadow-shadow tracking-wider "
                             key={index}>
-                            <p>{currentDate.toLocaleDateString()}</p>
+                            <p>{nepaliDate}</p>
                             <p>{(hourlyData.main.temp - 273.15).toFixed(2)}&deg;<span className="text-lg">C</span></p>
                             {hourlyData.weather.length > 0 && <p>{hourlyData.weather[0].main}</p>}
                         </div>
                     );
                 })}
-
             </div>
         </>
 
